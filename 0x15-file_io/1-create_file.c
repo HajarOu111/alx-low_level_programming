d679ba817e231aa7e390cfbe8a7d1e057bf1ec12
HAJAR OUAKRIM <assma941@gmail.com>
2023-05-09 10:09:50 -0700

    task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,18 +9,18 @@
 
 int create_file(const char *filename, char *text_content)
 {
-int fd, rwr, nl;
+int o, w, l = 0;
 if (filename == NULL)
 return (-1);
-fd = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-if (fd == -1)
+if (text_content != NULL)
+{
+for (l = 0; text_content[l];)
+l++;
+}
+o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+w = write(o, text_content, l);
+if (o == -1 || w == -1)
 return (-1);
-if (text_content == NULL)
-text_content = "";
-for (nl = 0; text_content[nl]; nl++)
-rwr = write(fd, text_content, nl);
-if (rwr == -1)
-return (-1);
-close(fd);
+close(o);
 return (1);
 }
