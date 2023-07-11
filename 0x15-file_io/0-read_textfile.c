ee87a2820451d68c7d63babe26dbe2a411be0bd5
HAJAR OUAKRIM <assma941@gmail.com>
2023-07-10 19:15:42 -0700

    task 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,30 +2,38 @@
 #include <stdlib.h>
 
 /**
- * read_textfile - a function that reads a text file and prints it to,
- *  the POSIX standard output.
- *  @filename: A pointer to the name of the file.
- *  @letters: The number of letters in the file to read.
- *  Return: returns 0 if the file is empty.
+ * read_textfile - Reads a text file and prints it to POSIX stdout.
+ * @filename: A pointer to the name of the file.
+ * @letters: The number of letters the
+ *           function should read and print.
+ *
+ * Return: If the function fails or filename is NULL - 0.
+ *         O/w - the actual number of bytes the function can read and print.
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-ssize_t a, b, c;
-char *buff;
-if (filename == NULL)
-return (0);
-buff = malloc(sizeof(char) * letters);
-if (buff == NULL)
-return (0);
-a = open(filename, O_RDONLY);
-b = read(a, buff, letters);
-c = write(STDOUT_FILENO, buff, b);
-if (a == -1 || b == -1 || c == -1 || b != c)
-{
-free(buff);
-return (0);
-}
-free(buff);
-close(a);
-return (0);
+	ssize_t o, r, w;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	buffer = malloc(sizeof(char) * letters);
+	if (buffer == NULL)
+		return (0);
+
+	o = open(filename, O_RDONLY);
+	r = read(o, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, r);
+
+	if (o == -1 || r == -1 || w == -1 || w != r)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(o);
+
+	return (w);
 }
