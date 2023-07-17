4f9481d5df5d7a964f2b1038405ebce766d79fc5
HAJAR OUAKRIM <assma941@gmail.com>
2023-07-16 18:06:12 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,19 +9,25 @@
  *  @b : the string that contains 0 and 1.
  *  Return: Returns 0 if b is NULL, returns a number otherwise.
  */
-unsigned int binary_to_uint(const char *b)
+ unsigned int binary_to_uint(const char *b)
 {
-int i, len;
-unsigned int n = 0;
-if (b == NULL)
-return (0);
-len = strlen(b);
-for (i = 0; i != len; i++)
-{
-if (b[len - i - 1] == '1')
-n += 1 << i;
-else if (b[len - i - 1] != '0')
-return (0);
-}
-return (n);
+unsigned int total, power;
+	int len;
+
+	if (b == NULL)
+		return (0);
+
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
+
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
 }
