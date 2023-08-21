a8bc8aa907495e571fba650335c10d764c54b196
HAJAR OUAKRIM <assma941@gmail.com>
2023-08-21 16:04:47 -0700

    task 0

---
+++ 0x18-dynamic_libraries/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: string
+ *Return: returns the length of string s
+ */
+
+
+
+
+
+
+
+int _strlen(char *s)
+{
+return (strlen(s));
+}
